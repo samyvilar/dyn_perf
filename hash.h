35dc3bd5c33c42daf2a8d60570470b6d06b3887d
Samy Vilar <samy.vilar@gmail.com>
2014-10-07 19:26:05 -0400

    removed dead code ....

--- hash.h
+++ hash.h
@@ -27,18 +27,29 @@
 ////      ^^^^^^^^^^^^^^^^^^ largest prime that is less than sqrt(2^64)
 
 #define max_prime(expr_or_type) scalr_switch_uint(expr_or_type, MAX_PRIME_64, MAX_PRIME_32, MAX_PRIME_16, MAX_PRIME_8, (void)0)
-//  ^^^^^^^^^^^^^^^^^^ returns the largest prime that is less than (2**bit_size(expr_or_type))
+//  ^^^^^^^^^^^^^^^^^^ returns the largest prime that is less than (2**bit_sz(expr_or_type))
 
 #define max_sqrt_prime(expr_or_type) \
     scalr_switch_uint(expr_or_type, MAX_SQRT_PRIME_64, MAX_SQRT_PRIME_32, MAX_SQRT_PRIME_16, MAX_SQRT_PRIME_8)
-// ^^^^^^^^^^^^^^^^^^^^^^^^^ returns the largest prime that is less than the sqrt(bit_size(expr_or_type)) ...
+// ^^^^^^^^^^^^^^^^^^^^^^^^^ returns the largest prime that is less than the sqrt(bit_sz(expr_or_type)) ...
 //                           required for universal hash functions ....
 
 
-#define rand_64 mt_rand_vect_64
-#define rand_32 mt_rand_vect_32
-#define rand_16 rand_32
-#define rand_8  rand_32
+#ifndef rand_64
+#   define rand_64 mt_rand_vect_64
+#endif
+
+#ifndef rand_32
+#   define rand_32 mt_rand_vect_32
+#endif
+#ifndef rand_16
+#   define rand_16 rand_32
+#endif
+#ifndef rand_8
+#   define rand_8  rand_32
+#endif
+
+//({*(unsigned long *)(int[2]){rand(), rand()};})
 
 #define hash_rand_coef(type_or_expr)    \
     ((scalr_switch_oblvs_sign_intgl(    \
@@ -50,67 +61,7 @@
         (void)0                         \
     ) % max_prime(type_or_expr)) | (typeof(type_or_expr))1)
 
-//// ** Universal Hash function first degree polynomial ...
-//#define _calc_univ_prod_hash(k, a, tbl_size) (((k) * (a)) >> ((typeof(tbl_size))bit_size(tbl_size) - leadn_one_index(tbl_size)))
-//#define _calc_univ_prod_pair_wise_hash(k, a, b, tbl_size)   \
-//    ((((k) * (a)) + (b)) >> ((typeof(tbl_size))bit_size(tbl_size) - leadn_one_index(tbl_size)))
-
-#define hash_univ_pow2(key, coef, shift_mag) ((key * coef) >> shift_mag)
-
-#define init_hash(self) ({                          \
-    (self)->coef = hash_rand_coef((self)->coef);    \
-    (self)->shift_mag = bit_size((self)->coef) - bits_leadn_one((self)->length);\
-})
-
-
-
-//#define rand_params(params, cnt) ({                     \
-//    typedef typeof(params[0]) _param_t;                 \
-//    _param_t                                            \
-//         *_dest = (params)                              \
-//        ,_parm_index                                    \
-//        ;                                               \
-//    for (_parm_index = 0; _parm_index < array_cnt(params); _parm_index++) \
-//        _dest[_parm_index] = ((                         \
-//            scalr_switch_oblvs_sign_intgl(              \
-//                *_dest,                                 \
-//                rand_64(),                              \
-//                rand_32(),                              \
-//                rand_16(),                              \
-//                rand_8(),                               \
-//                (void)0                                 \
-//            ) % max_prime(params[0])) | (_param_t)1);   \
-//    _dest; }) // ^^^^^^^^^^^^^^^^^^^^^ must be random odd integer
-//              //   that does not exceed the largest prime for its underlying bit size
-
-
-//    comp_select(                                                                                        \
-//        array_cnt(hash_params_coefs(hash_tbl_params(_tbl))) == 1LU,                                     \
-//        _calc_univ_prod_hash(_key, hash_params_coef(hash_tbl_params(_tbl), 0), hash_tbl_size(_tbl)),    \
-//    comp_select(                                                                                        \
-//        array_cnt(hash_params_coefs(hash_tbl_params(_tbl))) == 2LU,         \
-//        _calc_univ_prod_pair_wise_hash(                                     \
-//            _key,                                                           \
-//            hash_params_coef(hash_tbl_params(_tbl), 0),                     \
-//            hash_params_second_param(hash_tbl_params(_tbl)),                \
-//            hash_tbl_size(_tbl)                                             \
-//        ),                                                                  \
-//        (void)0                                                             \
-//    ))
-// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ assumes:
-//      - all operands are of an unsigned integral type with equivalent bit sizes ...
-//      - the size of the hash_tbl m, is a power of two, ie m === 2**M, M === log2(m),
-//      - each hash_param is an odd number, not exceeding the largest prime for its word size,
-//      - if giving a second hash_param b, then b < 2**(w - M), where w is the bit size of M
-// vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-// hash(x) = ((coef[0] * x + coef[1]) mod 2**w) /  2**(w-M)
-// hash(x) = (coef[0] * x + coef[1]) /  2**(w-M)
-// hash(x) = (coef[0] * x + coef[1]) >> (w - M),
-// hash(x) = (coef[0] * x + coef[1]) >> (bit_size(m) - log2(m))
-// hash(x) = (coef[0] * x + coef[1]) >> (bit_size(m) - leadn_one_index(m))
-// 4 instructions (mult, subtration, shift, leadn_one_index) at a minimun of 4-5 instructions,
-//      depending on the operands and compiler optimizations.
-// see: @http://en.wikipedia.org/wiki/Universal_hashing#Avoiding_modular_arithmetic
+#define hash_univ_pow2(key, coef, irlvnt_bits) (((key) * (coef)) >> (irlvnt_bits))
 
 
 #endif
