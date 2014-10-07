f4e8eb65bef5a7b9eb93633e0073a97dd804b02c
Samy Vilar <samy.vilar@gmail.com>
2014-10-07 19:21:17 -0400

    bug fix, made sure the empty_entry object isn't recycled by accident ...

--- entry.c
+++ entry.c
@@ -1,9 +1,22 @@
 
 
-
+#include "comp_utils.h"
 #include "entry.h"
 #include "alloc.h"
 
-entry_t *empty_entry = &(entry_t){.key = (entry_key_t)-1, .item = NULL};
+const entry_t *const empty_entry = &(entry_t){.key = (_t(((entry_t){}).key))-1};
 
-alloc_rec_templs(entry);
+//alloc_rec_templs(entry);
+
+alloc_mgt_obj(entry);
+alloc_templ(entry)
+release_tmpl(entry)
+
+inline void entry_recl(entry_t *const self) {
+    static entry_t **const prevs = (_t(prevs))&alloc_mgt(entry)._recld;
+
+    register const int is_good = (self != empty_entry); // <<<< don't recycle empty_entry!
+
+    self->_next = is_good ? *prevs : self->_next;
+    *prevs      = is_good ?  self  : *prevs;
+}
