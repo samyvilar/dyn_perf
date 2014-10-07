72b1f5c9b07066a0f0bb60e84878000f25cf9cc4
Samy Vilar <samy.vilar@gmail.com>
2014-10-07 19:11:22 -0400

    removed dead code, bug fixes, removed fld(set/get) in favor of faster flip which uses ^ to flip a bit ...

--- fld.c
+++ fld.c
@@ -3,7 +3,5 @@
 #include "bits.h"
 #include "alloc.h"
 
-fld_t *cleand_flds[bit_size(size_t)];
-
 alloc_rec_templs(fld);
 
--- fld.h
+++ fld.h
@@ -1,5 +1,3 @@
-
-
 #ifndef __FLD_H__
 #define __FLD_H__
 
@@ -9,85 +7,65 @@
 #include "bits.h"
 #include "comp_utils.h"
 
-
-#define bits_pow_2(expn) (((_t(expn))1) << (expn))
-
-#define fld_wrd_set(word, index) ((word) |=  (_t(word))bits_pow_2(index))
-#define fld_wrd_clr(word, index) ((word) &= ~(_t(word))bits_pow_2(index))
-#define fld_wrd_bit(word, index) ((word) &   (_t(word))bits_pow_2(index))
+#include "mem.h"
 
 typedef struct fld_t {
     unsigned short *words;
-    struct fld_t *_next;
+    struct fld_t   *_next;
 } fld_t;
+
 
 alloc_recl_sign_templs(fld);
 
-fld_t *cleand_flds[bit_size(size_t)];
+fld_t *cleand_flds[44]; // 1,099,511,627,776  16 bit words ...
 
-#define fld_calc_length(fld, capct) ((capct) / bit_size((fld)->words[0]))
+#define fld_calc_length(fld, capct) (((capct) / bit_sz((fld)->words[0])) + (((capct) % bit_sz((fld)->words[0])) != 0))
 
-static inline fld_t *fld_pow2_new(size_t capct) {
-    register fld_t **bucket = &cleand_flds[bits_leadn_one(capct)], *self;
+static_inline fld_t *fld_pow2_new(const unsigned char id) {
+    fld_t **bucket = &cleand_flds[id], *self;
 
     if (*bucket) {
         self = *bucket;
         *bucket = (*bucket)->_next;
-        self->words[0] = 0;
         return self;
     }
 
     self = fld_alloc();
+
+    const size_t capct = (size_t)1 << id;
     self->words = calloc(fld_calc_length(self, capct), _s(self->words[0]));
+
     return self;
 }
 
-static inline void fld_pow2_recl_clnd(fld_t *fld, size_t capct) {
-    register fld_t **bucket = &cleand_flds[bits_leadn_one(capct)];
+static_inline void fld_pow2_recl_clnd(fld_t *const fld, const unsigned char id) {
+    fld_t **const bucket = &cleand_flds[id];
     fld->_next = *bucket;
     *bucket = fld;
 }
 
-#define fld_wrd(fld, index) ((fld)->words[(index)/bit_size((fld)->words[0])])
 
-#define fld_bit_index(fld, index) ((index) % bit_size((fld)->words[0]))
-
-#define fld_get(fld, index) fld_wrd_bit(fld_wrd(fld, index), fld_bit_index(fld, index))
-#define fld_set(fld, index) fld_wrd_set(fld_wrd(fld, index), fld_bit_index(fld, index))
-#define fld_clr(fld, index) fld_wrd_clr(fld_wrd(fld, index), fld_bit_index(fld, index))
-
-
-#define fld_byt_comspt(fld, capct) (_s(*(fld)) + (_s((fld)->words[0]) * fld_calc_length(fld, capct)))
-
-static inline void fld_set_bits_indices(register const fld_t *const self, size_t capct, size_t *dest) {
-    register size_t curr, index;
-
-    for (curr = 0; curr < capct; curr++) {
-        _t(self->words[curr]) word = self->words[curr];
-
-        unsigned char cnt = bits_cnt_ones(word);
-
-        if (cnt & 1) { // if odd
-            *dest = bits_trln_one(word);
-            word >>= (*dest++ + 1);
-            cnt--;
-        }
-
-        for (; cnt; cnt -= 2) { // even number of set bits scan from the left and from the right ...
-            const unsigned char
-                leadng = bits_leadn_one(word),
-                trailn = bits_trlng_zrs(word);
-
-            *dest++ = (curr * bit_size(word)) + leadng;
-            *dest++ = (curr * bit_size(word)) + trailn;
-
-//            word &= ~((1 << leadng) | (1 << trailn));
-
-            word <<= (leadng + 1);
-            word >>= (leadng + trailn + 2);
-        }
+static const struct {
+    const _t(((fld_t){}).words[0]) word_t;
+    const struct {
+        const _t(((fld_t){}).words[0]) mask;
+        const _t(((fld_t){}).words[0]) bits_mag;
+    } id;
+} fld = {
+    .id = {
+        .mask       = bit_sz(fld.word_t) - 1,
+        .bits_mag   =
+            comp_select(_s(fld.word_t) == 1, 3,
+            comp_select(_s(fld.word_t) == 2, 4,
+            comp_select(_s(fld.word_t) == 4, 5,
+            comp_select(_s(fld.word_t) == 8, 6,
+                (void)0))))
     }
-}
+};
 
+#define fld_get(self, loc)  ((self)->words[(loc) / (bit_sz((self)->words[0]))] & ((_t((self)->words[0]))1 << ((loc) % (bit_sz((self)->words[0])))))
+#define fld_flip(self, loc) ((self)->words[(loc) / (bit_sz((self)->words[0]))] ^= ((_t((self)->words[0]))1 << ((loc) % (bit_sz((self)->words[0])))))
+
+#define fld_byt_comspt(fld, capct) ((_s((fld)->words[0]) * fld_calc_length(fld, capct)) + _s(*(fld)))
 
 #endif
