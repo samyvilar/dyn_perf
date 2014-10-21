9884ec4c59ab7b777da267b8424d1f3f920f9192
Samy Vilar <samy.vilar@gmail.com>
2014-10-21 17:52:18 -0400

    bug fixes, better search for randon unique keys ...

--- main.c
+++ main.c
@@ -12,62 +12,95 @@
 #include "dyn_perf.h"
 #include "entry.h"
 #include "mt_rand.h"
+#include "scalrs.h"
 
-int key_diff(const void *key0, const void *key1) {
-    return (int)(*(_t(((entry_t){}).key) *)key0 - *(_t(((entry_t){}).key) *)key1);
+int key_diff(const void *a, const void *b) {
+    return ((*(entry_t **)a)->key < (*(entry_t **)b)->key) ? -1 : 1;
 }
 
 void set_random_keys_and_values(
     _t(((entry_t){}).key)  *keys,
     _t(((entry_t){}).item) *values,
-    unsigned long cnt
+    const unsigned long cnt
 ) {
-    _t(keys) sorted_keys = malloc(sizeof(_t(*keys)) * cnt);
+    entry_t *entries = malloc(_s(*entries) * cnt);
+    entry_t **sorted_entries = malloc(_s(entries) * cnt);
     size_t index;
 
     static _t(((entry_t){}).key) (*const rand_key)() = (void *)
 #       ifdef __INTEL_COMPILER
             &rand_64
 #       else
-            comp_select(_s(*keys) == 4, &rand,
-            comp_select(_s(*keys) == 8, &rand_64,
-                (void)0))
+            comp_select(_s(*keys) < 8, &rand, &rand_64)
 #       endif
         ;
 
-    for (index = 0; index < cnt; index++) {
-        values[index] = (_t(*values))rand_64();
-        keys[index] = sorted_keys[index] = rand_key();
-//        keys[index] = sorted_keys[index] = (0x80000000ULL + ((size_t)index * 2));
-//        keys[index] = sorted_keys[index] = (index + 2);
+    for (index = cnt; index--; sorted_entries[index] = &entries[index])
+        entries[index] = (entry_t){.key = rand_key(), .item = (_t(*values)) rand_64()};
+
+    qsort(sorted_entries, cnt, _s(*sorted_entries), key_diff);
+
+    entry_t **duplcts = malloc(_s(entries) * cnt);
+    size_t curr, duplct_cnt = 0, range_cnt = 0;
+
+    struct {_t(*keys) low, high;} *avlbl_ranges = malloc((cnt + 2)* _s(*avlbl_ranges));
+
+    for (index = 0; index < cnt; index = curr) {
+        for (curr = index + 1; curr < cnt && sorted_entries[index]->key == sorted_entries[curr]->key; curr++, duplct_cnt++)
+            duplcts[duplct_cnt] = sorted_entries[curr];
+
+        if (curr < cnt) {
+            avlbl_ranges[range_cnt] = (_t(*avlbl_ranges)) {.low = sorted_entries[index]->key, .high = sorted_entries[curr]->key};
+            avlbl_ranges[range_cnt++].low++;
+        }
+    }
+    avlbl_ranges[0] = (_t(*avlbl_ranges)){.low = 0, .high = sorted_entries[0]->key};
+    avlbl_ranges[range_cnt] = (_t(*avlbl_ranges)){.low = sorted_entries[cnt - 1]->key, .high = 0};
+    avlbl_ranges[range_cnt].low++;
+    avlbl_ranges[range_cnt++].high--;
+
+    for (index = 0; duplct_cnt && index < range_cnt; index++) {
+        while (duplct_cnt && avlbl_ranges[index].low < avlbl_ranges[index].high)
+            duplcts[--duplct_cnt]->key = avlbl_ranges[index].low++;
     }
 
-    qsort(sorted_keys, cnt, sizeof(*keys), key_diff);
-    for (index = 1; index < cnt; index++)
-        if (sorted_keys[index] == sorted_keys[index - 1])
-            sorted_keys[index]++;
+    assert(duplct_cnt == 0);
+
+    qsort(sorted_entries, cnt, _s(*sorted_entries), key_diff);
+    for (index = 0; index < (cnt - 1); index++)
+        assert(sorted_entries[index]->key != sorted_entries[index + 1]->key);
 
     printf(
         "range: %'llu - %'llu\n"
-        ,(unsigned long long)sorted_keys[0]
-        ,(unsigned long long)sorted_keys[cnt - 1]
+        ,(unsigned long long)sorted_entries[0]->key
+        ,(unsigned long long)sorted_entries[cnt - 1]->key
     );
-    free(sorted_keys);
+
+    for (index = 0; index < cnt; index++) {
+        keys[index] = entries[index].key;
+        values[index] = entries[index].item;
+    }
+
+    free(entries);
+    free(sorted_entries);
+    free(duplcts);
+    free(avlbl_ranges);
 }
 
 
 int main()
 {
     setlocale(LC_NUMERIC, "");
-#   define TEST_SIZE 10000000
 
-    _t(((entry_t){}).key) *keys = malloc(TEST_SIZE * sizeof(*keys));
-    _t(((entry_t){}).item) *items = malloc(TEST_SIZE * sizeof(*items));
+    const size_t test_size = umin(10000000, bit_sz(((entry_t *)0)->key));
 
-    set_random_keys_and_values(keys, items, TEST_SIZE);
+    _t(((entry_t){}).key)  *keys  = malloc(test_size * sizeof(*keys));
+    _t(((entry_t){}).item) *items = malloc(test_size * sizeof(*items));
+
+    set_random_keys_and_values(keys, items, test_size);
 
     dyn_perf_t *perf = dyn_perf_new();
-    test_dyn_perf(perf, keys, items, TEST_SIZE);
+    test_dyn_perf(perf, keys, items, test_size);
 
     dyn_perf_recl(perf);
 
