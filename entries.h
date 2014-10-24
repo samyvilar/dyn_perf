3ca12467059022a2f3d19602aa6335a0cfbbb601
Samy Vilar <samy.vilar@gmail.com>
2014-10-24 18:00:35 -0400

    simpler new func, removed alignment requirement ...

--- entries.h
+++ entries.h
@@ -15,50 +15,47 @@ entry_t **cleand_entries[comp_select(_s(((entry_t){}).key) == 8, 45, bit_sz(((en
 // 2^44 ==  1.7 trillion entries
 
 
-static_inline entry_t **entries_pow2_align_alloc(const unsigned char id) {
-    return malloc_align(_s(entry_t *) * ((size_t)1 << id));
-}
-
 static_inline void entries_release_alloc_blocks() {
     unsigned char id;
     entry_t **curr, **next;
-    for (id = array_cnt(cleand_entries); id--; cleand_entries[id] = NULL)
-        for (curr = cleand_entries[id]; curr; curr = next) {
-            next = (void *)*(entry_t ***)curr;
-            free_align(curr);
+    for (id = array_cnt(cleand_entries); id--; cleand_entries[id] = NULL) {
+        for (curr = cleand_entries[id]; curr != NULL; curr = next) {
+            next = *(entry_t ***) curr;
+            free(curr);
         }
+    }
 }
 
 
-static_inline entry_t **entries_pow2_align_init(entry_t **self, const unsigned char id) {
+static_inline entry_t **entries_pow2_init(entry_t **const self, const unsigned char id) {
     typedef lrgst_vect_ingtl_t oprn_t;
 
-    void (*const store)(oprn_t *, oprn_t)   = vect.lrgst.intgl.ops->store_align;
+    void (*const store)(oprn_t *, oprn_t)   = vect.lrgst.intgl.ops->store;
     oprn_t (*const brdcst)(_t(empty_entry)) = vect.lrgst.intgl.ops->brdcst[_s(empty_entry)];
 
     const oprn_t pattrn = brdcst(empty_entry);
-    size_t len;
-    for (len = calc_len_log2(id, log2_frm_pow2[_s(oprn_t)/_s(word_t)]); len--; store(&((oprn_t *)self)[len], pattrn)) ;
+    size_t curr;
+    for (curr = calc_len_log2(id, log2_frm_pow2[_s(oprn_t)/_s(word_t)]); curr--; store(&((oprn_t *)self)[curr], pattrn))
+        ;
 
     return self;
 }
 
-static_inline _t(cleand_entries[0]) entries_pow2_new(const unsigned id)
+static_inline entry_t **entries_pow2_new(const unsigned char len_log2)
 {
-    _t(cleand_entries[0]) self =
-        cleand_entries[id] ? cleand_entries[id]
-                           : entries_pow2_align_init(entries_pow2_align_alloc(id), id);
+    if (cleand_entries[len_log2]) {
+        entry_t **self = cleand_entries[len_log2];
+        cleand_entries[len_log2] = *(entry_t ***)self;
+        *self = (void *)empty_entry;
+        return self;
+    }
 
-    cleand_entries[id] = cleand_entries[id] ? (void *)*self : cleand_entries[id];
-    //                   ^^^^^^^^^^^^^^^^^^ if there was a block available, remove it, otherwise do nothing ...
-
-    *self = (void *)empty_entry;
-    return self;
+    return entries_pow2_init(malloc(_s(entry_t *) * (1ULL << len_log2)), len_log2);
 }
 
-static_inline void entries_pow2_recl_cleand(entry_t **self, const unsigned char id) {
-    *(entry_t ***)self = cleand_entries[id];
-    cleand_entries[id] = self;
+static_inline void entries_pow2_recl_cleand(entry_t **self, const unsigned char len_log2) {
+    *(entry_t ***)self = cleand_entries[len_log2];
+    cleand_entries[len_log2] = self;
 }
 
 #endif
