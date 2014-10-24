a3534edf744e91aeefd12996db016bd7b72bf57b
Samy Vilar <samy.vilar@gmail.com>
2014-10-24 18:53:53 -0400

    fallback to default object management ...

--- entry.c
+++ entry.c
@@ -6,15 +6,4 @@
 
 const entry_t *const empty_entry = &(entry_t){.key = (_t(((entry_t){}).key))-1};
 
-alloc_obj_tdef_tmpl(entry);
-alloc_mgt_obj(entry);
-alloc_templ(entry)
-release_tmpl(entry)
-
-inline void entry_recl(entry_t *const self) {
-//    if (self == empty_entry)
-//        return ;
-
-    *(entry_t **)self = alloc_mgt(entry).recld;
-    alloc_mgt(entry).recld = self;
-}
+alloc_rec_templs(entry);
