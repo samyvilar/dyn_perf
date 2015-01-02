ea515fe1e81781398641d503004ff9ed657f0605
Samy Vilar <samy.vilar@gmail.com>
2015-01-02 18:07:34 -0500

    added avx, and simd2, still need to figure out a way to inline when using icc, since it refuses no matter what flags or attributes are used ...

--- vect.h
+++ vect.h
@@ -5,27 +5,54 @@
 #include <immintrin.h>
 
 #include "comp_utils.h"
-#include "scalrs.h"
 
+#include "simd2.h"
 #include "sse2.h"
+#include "avx2.h"
 
+#if defined(__AVX2__)
+#   define vect_lrgst_singl_type    __m256
+#   define vect_lrgst_doubl_type    __m256d
 
-#define vect_lrgst_intgl_type __m128i
+#   define vect_lrgst_intgl_type    __m256i
+#   define vect_lrgst_intgl_oprts   &avx.ops.intgl
+#elif defined(__SSE2__)
+#   define vect_lrgst_singl_type    __m128
+#   define vect_lrgst_doubl_type    __m128d
 
+#   define vect_lrgst_intgl_type    __m128i
+#   define vect_lrgst_intgl_oprts   &sse.ops.intgl
+#else
+#   define vect_lrgst_singl_type    simd64
+#   define vect_lrgst_doubl_type    simd64d
+
+#   define vect_lrgst_intgl_type    simd64i
+#   define vect_lrgst_intgl_oprts   &simd.ops.intgl
+#endif
 
 static const struct {
-    const _t(sse) *const sse;
+    const _t(sse)  *const sse;
+    const _t(avx)  *const avx;
+    const _t(simd) *const simd;
 
     const struct {
         const struct {
-            _t(sse.types.intgl) type;
-            const _t(sse.ops.intgl) *const ops;
+#       if   defined(__AVX2__)
+            const _t(avx.ops.intgl)
+#       elif defined(__SSE2__)
+            const _t(sse.ops.intgl)
+#       else
+            const _t(simd.ops.intgl)
+#       endif
+            *const ops;
         } intgl;
     } lrgst;
-
 } vect = {
-    .sse             = &sse,
-    .lrgst.intgl.ops = &sse.ops.intgl
+    .sse    = &sse,
+    .avx    = &avx,
+    .simd   = &simd,
+
+    .lrgst.intgl.ops = vect_lrgst_intgl_oprts
 };
 
 #endif
