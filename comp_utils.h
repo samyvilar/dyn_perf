4e7c6c1eece1fac2f98ec248179c3c01ef480441
Samy Vilar <samy.vilar@gmail.com>
2015-01-02 18:05:46 -0500

    removed unused macros, still need to fix array_cnt, since it doesn't assert the type is indeed an array ...

--- comp_utils.h
+++ comp_utils.h
@@ -4,6 +4,7 @@
 
 #include <assert.h>
 #include <stddef.h>
+#include <limits.h>
 
 #ifndef _t
 #   define _t typeof
@@ -14,15 +15,11 @@
 #endif
 
 #ifndef bit_sz
-#   define bit_sz(type) (_s(type) * 8)
+#   define bit_sz(type) (_s(type) * CHAR_BIT)
 #endif
 
 #ifndef bit_offst
 #   define bit_offst(tp, memb) (offsetof(_t(tp), memb) * CHAR_BIT)
-#endif
-
-#ifndef array_cnt
-#   define array_cnt(arr) (_s(arr)/_s((arr)[0]))
 #endif
 
 
@@ -30,12 +27,12 @@
 #ifdef __INTEL_COMPILER
 #   define intrsc_attrs __attribute__((__gnu_inline__, __always_inline__, __artificial__)) //, __artificial__
 #else
-#   define intrsc_attrs __attribute__((__gnu_inline__, __always_inline__, __nodebug__)) //, __artificial__
+#   define intrsc_attrs __attribute__((__gnu_inline__, __always_inline__)) //, __artificial__
 #endif
 #endif
 
 #ifndef static_inline
-#   define static_inline static __inline__
+#   define static_inline static __inline__ intrsc_attrs
 #endif
 
 
@@ -55,15 +52,16 @@
 //      ^^^^^^^^^^^^^^^^ returns 0 if expression is a compile time constant otherwise 0 ...
 //              compilation error if argument is not an expression ...
 
-#define comp_t_is_array(arg)  comp_t_eq(_t(arg[0]) [], _t(arg))
+#define comp_t_is_array(arg)  comp_t_eq(_t((arg)[0]) [], _t(arg))
 //      ^^^^^^^^^^^^^ returns 1 if arg is an array of some sort, 0 otherwise (ie its a pointer, scalar, obj, ...)
 
 #define comp_t_is_void_ptr(arg) comp_t_eq(_t(arg), void *)
 //      ^^^^^^^^^^^^^^^^ returns 1 if arg is a void pointer, two pointers to diff types are considered diff ...
 
 
-#define comp_unlikely(expr) __builtin_expect(expr, 0)
-#define comp_likely(expr)   __builtin_expect(expr, 1)
+#ifndef array_cnt
+#   define array_cnt(arr) (_s(arr)/_s((arr)[0]))
+#endif
 
 
 #endif
